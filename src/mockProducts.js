a8f0ff1c4c886598e33e05303c8e95cb3b328561
Martha-K <110817385+Martha-K@users.noreply.github.com>
2024-02-05 16:51:00 +0200

    locked data to create product cards

--- src/mockProducts.js
+++ src/mockProducts.js
@@ -1,21 +1,70 @@
-
+import armature from "./img/armature.jpg"
 
 [
   {
     armature: [
       {
-        name: "",
-        image,
-        price: 24
-        
+        name: "Armature",
+        image: {armature},
+        price: 24,
+        inStock: true,
+        id: "1"
       },
       {
-        name: "",
-        image,
+        name: "Armature",
+        image: {armature},
+        price: 24,
+        inStock: true,
+        id: "2"
+
+      },
+       {
+        name: "Armature",
+        image: {armature},
+        price: 24,
+        inStock: true,
+        id: "3"
+
+      },
+       {
+        name: "Armature",
+        image: {armature},
+        price: 24,
+        inStock: true,
+        id: "4"
+
       },
     ],
   },
   {
-    grid: [{ name: "", image: "" }],
+    grid: [ {
+        name: "Grid",
+        image: {armature},
+        price: 24,
+        inStock: true,
+        id: "5"
+
+      }, {
+        name: "Grid",
+        image: {armature},
+        price: 24,
+        inStock: true,
+        id: "6"
+
+      }, {
+        name: "Grid",
+        image: {armature},
+        price: 24,
+        inStock: true,
+        id: "7"
+
+      }, {
+        name: "Grid",
+        image: {armature},
+        price: 24,
+        inStock: true,
+        id: "8"
+
+      },],
   },
 ];
