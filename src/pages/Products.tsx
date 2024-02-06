ab5228c17aa1bcef054306e22ee25eaad015ae60
Martha-K <110817385+Martha-K@users.noreply.github.com>
2024-02-07 01:35:12 +0200

    markup of the products page is done

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,22 +1,66 @@
-import { Div } from "./ProductsStyles";
+import React, { useState } from "react";
+import {
+  Img,
+  CardWrapper,
+  Card,
+  ButtonWrapper,
+  ButtonContainer,
+  ButtonStyles,
+  Div,
+  ProductName,
+  CardInfoWrapper,
+  ProductWrapping,
+} from "./ProductsStyles";
 import { mockProduct } from "../mockProducts";
 
 export const Products = () => {
+  const [isArmature, setIsArmature] = useState(true);
+  console.log("isArmature", isArmature);
+  const armatureSelect = () => {
+    !isArmature && setIsArmature(true);
+  };
+  const gridSelect = () => {
+    isArmature && setIsArmature(false);
+  };
+
   return (
-    <Div>
-      {mockProduct?.map((el) => (
-        <div key={Object.keys(el)[0]}>
-          <h2>{(el?.armature || el?.grid)?.[0]?.name}</h2>
-          {(el?.armature || el?.grid).map((el) => (
-            <div key={el.name}>
-              <h3>{el.name}</h3>
-              <img src={el.image} alt="product" />
-              <p>Price: {el.price}</p>
-              {el.inStock && <p>В наявності</p>}
-            </div>
-          ))}
-        </div>
-      ))}
-    </Div>
+    <div>
+      <ButtonContainer>
+        <ButtonStyles data-active={isArmature}>
+          <Div>
+            <ButtonWrapper
+              onClick={armatureSelect}
+              data-image="armature"
+            ></ButtonWrapper>
+            <ProductName>Armature</ProductName>
+          </Div>
+        </ButtonStyles>
+        <ButtonStyles data-active={!isArmature}>
+          <Div>
+            <ButtonWrapper onClick={gridSelect} data-image="grid">
+              <ProductName>Grid</ProductName>
+            </ButtonWrapper>
+          </Div>
+        </ButtonStyles>
+      </ButtonContainer>
+      <ProductWrapping>
+        {mockProduct?.map((el) => (
+          <CardWrapper>
+            {el[isArmature ? "armature" : "grid"]?.map((item) => (
+              <Card key={item.id}>
+                <h2>{item.name}</h2>
+                <CardInfoWrapper>
+                  <Img src={item.image} />
+                  <div>
+                    <p>Ціна: {item.price}</p>
+                    <p>{item.inStock ? "в наявності" : "немає в наявності"}</p>
+                  </div>
+                </CardInfoWrapper>
+              </Card>
+            ))}
+          </CardWrapper>
+        ))}
+      </ProductWrapping>
+    </div>
   );
 };
--- src/pages/ProductsStyles.js
+++ src/pages/ProductsStyles.js
@@ -1,5 +1,117 @@
 import styled from "styled-components";
+import slide3 from "../img/slide3.jpg";
+import slide from "../img/slide.jpg";
+
+export const ButtonWrapper = styled.div`
+  position: relative;
+  background-image: ${(props) =>
+    props["data-image"] === "armature" ? `url(${slide3})` : `url(${slide})`};
+  width: 560px;
+  height: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 20px;
+  filter: blur(5px);
+`;
 
 export const Div = styled.div`
-  /* background-color: blue; */
+  overflow: hidden;
+`;
+
+export const ProductName = styled.p`
+  position: absolute;
+  font-size: 60px;
+  right: 70%;
+  bottom: 50%;
+  color: white;
+`;
+
+export const ButtonStyles = styled.div`
+  /* background-color: #8fa8c7; */
+  border-radius: 20px;
+  padding: 30px;
+  padding-bottom: ${(props) => (props["data-active"] ? "100px" : "30px")};
+  background-color: ${(props) =>
+    props["data-active"] ? "lightblue" : "#8fa8c7"};
+     transition: all 0.3s ease-in-out;
+
+
+&:hover {
+  transform: scale(1.02);
+  box-shadow: 20px 20px 10px rgba(0, 0, 0, 0.5);
+}
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  margin-top: 10px;
+  gap: 100px;
+  justify-content: center;
+  align-items: start;
+`;
+
+export const Img = styled.img`
+  width: 260px;
+  border-radius: 20px;
+
+  @media (max-width: 550px) {
+    width: 160px;
+  }
+`;
+
+export const ProductWrapping = styled.div`
+  background-color: lightblue;
+  padding: 40px;
+`;
+
+export const CardInfoWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+
+  @media (max-width: 400px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 10%;
+  }
+`;
+
+export const CardWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 0.75fr));
+  grid-gap: 40px;
+
+  @media (max-width: 900px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 10%;
+  }
+`;
+
+export const Card = styled.div`
+  margin: 0;
+  padding: 20px;
+  background-color: #f5f5f5;
+  box-shadow: 8px 8px 10px rgba(0, 0, 0, 0.5);
+  border-radius: 20px;
+  transition: all 0.3s ease-in-out;
+
+
+  &:hover {
+    transform: scale(1.02);
+    box-shadow: 20px 20px 10px rgba(0, 0, 0, 0.5);
+  }
+
+  @media (max-width: 900px) {
+    max-width: 650px;
+    width: 100%;
+  }
+  @media (max-width: 400px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 10%;
+  }
 `;
